01b2a84f95811e5916cf95ea58bfc8d63b244ca6
wmamul <wojciech.mamul@gmail.com>
2020-03-27 22:16:30 +0100

    Finished password reset functionality

--- django-blog/settings.py
+++ django-blog/settings.py
@@ -11,22 +11,26 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import json
+
+# Open json config file
+
+with open('/etc/bloglive-config.json') as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-3amm_-hk2gf23n8o=h(@5o#3er6%_s&e3efnnp1u^%gn27*%q'
+SECRET_KEY = config.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
-
+ALLOWED_HOSTS = config.get('ALLOWED_HOSTS')
 
 # Application definition
 
@@ -132,4 +136,6 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBacked'
-#EMAIL_HOST 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USER = config.get('EMAIL_USER')
+EMAIL_PASS = config.get('EMAIL_PASS')
